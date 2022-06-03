f6d5e002ef6271f3d4ecc9224e83b8e19d373519
ISMAEL <ismailelhadad3@gmail.com>
2022-06-03 15:29:23 +0100

    Question 7

---
+++ app/src/main/java/com/example/cctdi201/ListeActivite.java
@@ -0,0 +1,14 @@
+package com.example.cctdi201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeActivite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_activite);
+    }
+}
--- app/src/main/java/com/example/cctdi201/MainActivity.java
+++ app/src/main/java/com/example/cctdi201/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cctdi201;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void affichage(View view) {
+        Intent i=null;
+        switch (view.getId()){
+            case R.id.btn1:i=new Intent(MainActivity.this,AjouterSociete.class);break;
+            case R.id.btn2:i=new Intent(MainActivity.this,ModifierSociete.class);break;
+            case R.id.btn3:i=new Intent(MainActivity.this,ListeActivite.class);break;
+
+        }
+        startActivity(i);
     }
 }
