0a674a7ad55a2228a3f49d62276289797e2fad91
ISMAEL <ismailelhadad3@gmail.com>
2022-06-03 16:42:03 +0100

    Question 12

--- app/src/main/java/com/example/cctdi201/ListeActivite.java
+++ app/src/main/java/com/example/cctdi201/ListeActivite.java
@@ -3,12 +3,32 @@ package com.example.cctdi201;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ListeActivite extends AppCompatActivity {
-
+    ListView lst;
+    MyDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_activite);
+        lst=findViewById(R.id.ls);
+        db=new MyDatabase(this);
+        ArrayList<Societe> s =MyDatabase.getAllSociete(db.getReadableDatabase());
+        ArrayList<HashMap<String,Object>> h = new ArrayList<>();
+        for(Societe so : s){
+            HashMap<String,Object> has =new HashMap<>();
+            has.put("nom",so.getNom());
+            has.put("nb",so.getNombreemploye());
+            h.add(has);
+        }
+        String[] from = {"nom","nb"};
+        int [] to ={R.id.trai,R.id.tcap};
+        SimpleAdapter sa = new SimpleAdapter(this,h,R.layout.societes,from,to);
+        lst.setAdapter(sa);
     }
 }
