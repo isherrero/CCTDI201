2aa48663349cdaf0435b5bac41d0074119fcc715
ISMAEL <ismailelhadad3@gmail.com>
2022-06-03 15:00:18 +0100

    Question 4

--- app/src/main/java/com/example/cctdi201/MyDatabase.java
+++ app/src/main/java/com/example/cctdi201/MyDatabase.java
@@ -6,13 +6,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDatabase extends SQLiteOpenHelper {
     private static String DB_NAME="societes.db";
+    private static String TB_NAME="societe";
+    private static String COL1="id";
+    private static String COL2="raison_sociale";
+    private static String COL3="secteur_activite";
+    private static String COL4="nb_employe";
 
     public MyDatabase(Context c){
         super(c,DB_NAME,null,1);
     }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String req="CREATE TABLE "+TB_NAME+"("+COL1 +" integer primary key autoincrement,"+COL2 +" text,"+COL3+" text,"+COL4 +"integer)";
+        sqLiteDatabase.execSQL(req);
     }
 
     @Override
